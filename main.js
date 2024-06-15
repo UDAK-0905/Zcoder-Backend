49d3b075dbf56ea97c4dfea2ac2b21322f2c20c5
Sanyam243 <129854450+Sanyam243@users.noreply.github.com>
2024-06-15 11:26:58 +0530

    coikie session

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const main = express();
 const multer = require("multer");
-const session = require('express-session');
+const session = require('cookie-session');
 
 
 
